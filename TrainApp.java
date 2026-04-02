3a23c70d33e8b954ff0919e5c8dda82909035be0
akashsingh2127 <akash.vidyagyan11@gmail.com>
2026-04-02 09:30:30 +0530

    UC9: Group bogies using Stream groupingBy collector

--- TrainApp.java
+++ TrainApp.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 class Bogie {
@@ -23,15 +24,20 @@ public class TrainApp {
         bogies.add(new Bogie("Sleeper", 72));
         bogies.add(new Bogie("AC Chair", 60));
         bogies.add(new Bogie("First Class", 24));
+        bogies.add(new Bogie("Sleeper", 70));
 
-        List<Bogie> filteredBogies = bogies.stream()
-                                           .filter(b -> b.capacity > 60)
-                                           .collect(Collectors.toList());
+        Map<String, List<Bogie>> groupedBogies =
+                bogies.stream()
+                      .collect(Collectors.groupingBy(b -> b.name));
 
-        System.out.println("Filtered Bogies (Capacity > 60):");
+        System.out.println("Grouped Bogies:");
 
-        for (Bogie b : filteredBogies) {
-            System.out.println(b.name + " : " + b.capacity);
+        for (Map.Entry<String, List<Bogie>> entry : groupedBogies.entrySet()) {
+            System.out.println(entry.getKey() + " :");
+
+            for (Bogie b : entry.getValue()) {
+                System.out.println("  " + b.capacity);
+            }
         }
     }
 }
