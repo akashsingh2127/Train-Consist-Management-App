171b60a66757940e15f2fab4dc4959c3ae44f2bc
akashsingh2127 <akash.vidyagyan11@gmail.com>
2026-04-02 09:34:21 +0530

    UC10: Aggregate seating capacity using map and reduce

--- TrainApp.java
+++ TrainApp.java
@@ -1,7 +1,5 @@
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 class Bogie {
     String name;
@@ -24,20 +22,11 @@ public class TrainApp {
         bogies.add(new Bogie("Sleeper", 72));
         bogies.add(new Bogie("AC Chair", 60));
         bogies.add(new Bogie("First Class", 24));
-        bogies.add(new Bogie("Sleeper", 70));
 
-        Map<String, List<Bogie>> groupedBogies =
-                bogies.stream()
-                      .collect(Collectors.groupingBy(b -> b.name));
+        int totalCapacity = bogies.stream()
+                                  .map(b -> b.capacity)
+                                  .reduce(0, Integer::sum);
 
-        System.out.println("Grouped Bogies:");
-
-        for (Map.Entry<String, List<Bogie>> entry : groupedBogies.entrySet()) {
-            System.out.println(entry.getKey() + " :");
-
-            for (Bogie b : entry.getValue()) {
-                System.out.println("  " + b.capacity);
-            }
-        }
+        System.out.println("Total Seating Capacity: " + totalCapacity);
     }
 }
